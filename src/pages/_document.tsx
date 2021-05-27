c0499a6306a56d8e24aa3715afbce761d917aaed
lucas <lucasbotaro1@gmail.com>
2021-05-27 15:44:58 -0300

    Criado componente Header da aplicacao e adicionado imagens a aplicacao.

---
+++ src/pages/_document.tsx
@@ -0,0 +1,26 @@
+// Comparado ao index.html na pagina public do CRA
+// Somente sera renderizado uma unica vez
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
+            rel="stylesheet"
+          />
+
+          <link rel="shortcut icon" href="favicon.png" type="image/png" />
+        </Head>
+        <body>
+          <Main />
+          {/** Arquivos JS que a aplicacao precisara para rodar, eh preciso colocar no final */}
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
