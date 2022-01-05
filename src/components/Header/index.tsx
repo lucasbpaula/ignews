bcbe3e9aa11e8097392a5453b986905d6ba0092f
lucas <lucasbotaro1@gmail.com>
2022-01-04 22:11:59 -0300

    feat: alterado componente Header e criado componente ActiveLink

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,23 @@
-import SignInButton from "../SignInButton";
-import styles from "./styles.module.scss";
+import { ActiveLink } from '../ActiveLink';
+import SignInButton from '../SignInButton';
+import styles from './styles.module.scss';
 
 export default function Header() {
-  return (
-    <header className={styles.headerContainer}>
-      <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
-        <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
-        </nav>
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <img src="/images/logo.svg" alt="ig.news" />
+                <nav>
+                    <ActiveLink href="/" activeClassName={styles.active}>
+                        <a className={styles.active}>Home</a>
+                    </ActiveLink>
+                    <ActiveLink href="/posts" activeClassName={styles.active}>
+                        <a>Posts</a>
+                    </ActiveLink>
+                </nav>
 
-        <SignInButton />
-      </div>
-    </header>
-  );
+                <SignInButton />
+            </div>
+        </header>
+    );
 }
